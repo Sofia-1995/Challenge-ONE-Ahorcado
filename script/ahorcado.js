eeeb7132c6b4a8b7ddd94cd27564a739b08c2e9e
Sofi <sm.pereyra95@gmail.com>
2022-06-05 17:09:33 -0300

    Pinto letras correctas

--- script/ahorcado.js
+++ script/ahorcado.js
@@ -15,27 +15,75 @@ var boxLetrasCorrectas = document.querySelector("#letras-correctas");
 var listaPalabras = ["AHORCADO", "ARBOL", "PALABRA", "JUEGO", "PERRO", "GATO"];
 var palabraSecreta = "";
 var letrasCliqueadas = [];
+var listaLetras = [];
 
 // Juego
-function validarLetra(letra) {
-  var esCaracter = letra.length === 1;
+function validarTecla(letra) {
+  // pregunto si la tecla es valida
+  // que no sea un espacio o enter, o escape, etc
+  // y que no haya sido clickeada antes
+  // por ahora si entran numeros y simbolos
+  var esTeclaValida = letra.length === 1;
   var esEspacio = letra === " ";
   var yaCliqueado = letrasCliqueadas.indexOf(letra) >= 0;
-  if (esCaracter && !esEspacio && !yaCliqueado) {
-    letrasCliqueadas.push(letra);
-    return true;
-  } else {
-    letrasCliqueadas.push(letra);
-    return false;
-  }
+
+  if (yaCliqueado || esEspacio) return false;
+
+  letrasCliqueadas.push(letra);
+
+  return esTeclaValida;
+}
+
+function validarLetra(letra) {
+  // chequeo si la letra existe dentro de la palabra
+  return palabraSecreta.indexOf(letra) >= 0;
+}
+
+function pintarLetrasCorrectas(letraCliqueada) {
+  // yo pinto si o si
+  // pero necesito saber
+  // que pinto (letraCliqueada)
+  // donde lo pinto (posicion de la letra)
+  console.log("pinto letras!");
+
+  // recorro la lista de letras
+  listaLetras.forEach(function (letra, index) {
+    // preguntar si las letras coinciden
+    // donde coincidan, utilizo la posicion (index)
+    if (letra === letraCliqueada) {
+      // creo un span con la letra
+      var spanLetra = document.createElement("span");
+      spanLetra.textContent = letraCliqueada;
+
+      //obtener la lista de cajas
+      var listaCajas = document.querySelectorAll(".letra-caja");
+
+      // meto el span en la caja que le corresponde segun el index
+      listaCajas[index].appendChild(spanLetra);
+
+      console.log(listaCajas);
+      console.log(`pinto la letra ${letraCliqueada} en la posicion ${index}`);
+    }
+  });
+}
+
+function pintarLetraIncorrecta(letra) {
+  console.log("pinto letra incorrecta!");
+}
+
+function perderIntento(letra) {
+  console.log("pinto letra incorrecta!");
 }
 
 function detectarInput(event) {
-  console.log("hola");
-  console.log(event.key);
-  console.log(letrasCliqueadas);
-  if (validarLetra(event.key)) {
-    console.log("entro al if");
+  var letra = event.key.toUpperCase();
+  if (validarTecla(letra)) {
+    if (validarLetra(letra)) {
+      pintarLetrasCorrectas(letra);
+    } else {
+      pintarLetraIncorrecta(letra);
+      perderIntento();
+    }
   }
 }
 
@@ -47,10 +95,10 @@ function inicializar() {
   palabraSecreta = palabra;
   console.log(palabra);
 
-  // pintar lineas
-  var listaLetras = [];
+  // guardo la lista de letras de la palabra elegida
   listaLetras = palabra.split("");
-  console.log(listaLetras);
+
+  // pintar lineas
   listaLetras.map(function () {
     var item = document.createElement("div");
     item.classList.add("letra-caja");
@@ -59,7 +107,12 @@ function inicializar() {
   document.addEventListener("keydown", detectarInput);
 }
 
-function resetJuego() {}
+function resetJuego() {
+  document.removeEventListener("keydown", detectarInput);
+  palabraSecreta = "";
+  letrasCliqueadas = [];
+  listaLetras = [];
+}
 
 // Navegacion
 function abrirJuego() {
@@ -83,7 +136,7 @@ function volverHome() {
   palabraPage.classList.add("hidden");
   /*mostrar la home*/
   homePage.classList.remove("hidden");
-  document.removeEventListener("keydown", detectarInput);
+  resetJuego();
 }
 
 buttonJuego.addEventListener("click", abrirJuego);
